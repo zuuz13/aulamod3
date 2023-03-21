cf32845a4eceaf7bf537c5c06d3edb86a5bf1654
Eder Sarmento <eder.13zuuz@gmail.com>
2023-03-20 22:07:59 -0300

    comentarios

--- src/ex3.js
+++ src/ex3.js
@@ -1,13 +1,13 @@
-const Pessoa_aqui = require ('./classPessoa')
+const Pessoa_aqui = require('./classPessoa')
 
-const pessoa = new Pessoa_aqui('testeNome', 'testeSobrenome'); 
+const pessoa = new Pessoa_aqui('testeNome', 'testeSobrenome');
 
 console.log(pessoa.nome); // testeNome 
 console.log(pessoa.sobrenome); // testeSobrenome 
 
 console.log("======================"); // testeSobrenome 
 
- 
- Object.keys(pessoa).forEach(key => { // cria indice chamado key para os iens do objeto
-    console.log(key, pessoa[key]);    // exibe a lista de conteúdo do objeto em sequencia
-  });
+
+Object.keys(pessoa).forEach(key => { // cria indice chamado key para os iens do objeto
+  console.log(key, pessoa[key]);    // exibe a lista de conteúdo do objeto em sequencia
+});
